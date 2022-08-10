6e331234268ecd9efd5b715c58ed0fb588317b8a
katheryn-k <kirichenko.ekaterynaa@yandex.ru>
2022-08-10 17:23:45 +0300

    feature activeSlide function

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const slides = document.querySelectorAll('.slide');
+function activeSlide(active = 1) {
+    const slides = document.querySelectorAll('.slide');
 
-slides.forEach(item => {
-    item.addEventListener('click', () => {
-        removeActiveClasses();
-        item.classList.add('active');
-    });
-});
+    slides[active].classList.add('active');
 
-function removeActiveClasses() {
     slides.forEach(item => {
-        item.classList.remove('active');
+        item.addEventListener('click', () => {
+            removeActiveClasses();
+            item.classList.add('active');
+        });
     });
+
+    function removeActiveClasses() {
+        slides.forEach(item => {
+            item.classList.remove('active');
+        });
+    };
 };
+
+
+
+activeSlide();
